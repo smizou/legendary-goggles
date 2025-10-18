0482950fb95bf6dc48ec7fb29084749b66fae976
smizou <158730900+smizou@users.noreply.github.com>
2025-10-18 23:19:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
     formSelector: '[data-dynamic-form]',
-    apiEndpoint: '/.netlify/functions/submit-mail-table',
+    apiEndpoint: '/.netlify/functions/submit',
     recaptchaSiteKey: '6Lfgft8rAAAAAF7IiVk0-LjPvlGvu29VO8j8eE5r',
     deliveryDataFile: '../json/_data.json'
 };
